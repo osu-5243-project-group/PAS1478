da98a43d89db2470f8144185b72ff69239654bd5
Alyssa neubauer <neubauer.30@osu.edu>
2019-03-27 10:42:50 -0400

    adding the boddies classification functions

--- classify.py
+++ classify.py
@@ -1,5 +1,5 @@
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,29 +24,55 @@ def get_data(data_filename,labels_filename):
 			labels.append(l[0])
 	return data,labels
 
-def main():
-	topic_data,topic_labels=get_data('reduced_data/reduced_titles.out','reduced_data/topics_labels.out')
-	topic_train_data=topic_data[0:11000]
-	topic_test_data=topic_data[11001:11366]
-	topic_train_labels=topic_labels[0:11000]
-	topic_test_labels=topic_labels[11001:11366]
 
-	topic_vectorizer = CountVectorizer()
-	topic_X = topic_vectorizer.fit_transform(topic_train_data)
-	topic_Y= topic_vectorizer.transform(topic_test_data)
 
-	neigh = KNeighborsClassifier(n_neighbors=6)
-	neigh.fit(topic_X, topic_train_labels)
+def classifyBodiesTopicsPlaces():
+   topic_data,topic_labels=get_data('reduced_data/reduced_bodies.out','reduced_data/topics_labels.out')
+   topic_train_data=topic_data[0:11000]
+   topic_test_data=topic_data[11001:11366]
+   topic_train_labels=topic_labels[0:11000]
+   topic_test_labels=topic_labels[11001:11366]
 
-	topic_predicted_labels=neigh.predict(topic_Y)
+   topic_vectorizer = CountVectorizer()
+   topic_X = topic_vectorizer.fit_transform(topic_train_data)
+   topic_Y= topic_vectorizer.transform(topic_test_data)
 
-	print(topic_predicted_labels)
-	topic_classificationReport = classification_report(topic_predicted_labels, topic_test_labels)
-	print("Classification report for testing set: ")
-	print(topic_classificationReport)
+   neigh = KNeighborsClassifier(n_neighbors=6)
+   neigh.fit(topic_X, topic_train_labels)
 
-	print("Accuracy score for testing set: ")
-	print(accuracy_score(topic_predicted_labels, topic_test_labels))
+   topic_predicted_labels=neigh.predict(topic_Y)
+
+   print(topic_predicted_labels)
+   topic_classificationReport = classification_report(topic_predicted_labels, topic_test_labels)
+   print("Classification report for testing set: ")
+   print(topic_classificationReport)
+
+   print("Accuracy score for testing set: ")
+   print(accuracy_score(topic_predicted_labels, topic_test_labels))
+
+   places_data,places_labels=get_data('reduced_data/bodies.out','reduced_data/places_labels.out')
+   places_train_data=places_data[0:11000]
+   places_test_data=places_data[11001:11366]
+   places_train_labels=places_labels[0:11000]
+   places_test_labels=places_labels[11001:11366]
+
+   places_vectorizer = CountVectorizer()
+   places_X = topic_vectorizer.fit_transform(places_train_data)
+   places_Y= topic_vectorizer.transform(places_test_data)
+
+   neigh = KNeighborsClassifier(n_neighbors=1)
+   neigh.fit(places_X, places_train_labels)
+
+   places_predicted_labels=neigh.predict(places_Y)
+
+   places_classificationReport = classification_report(places_predicted_labels, places_test_labels)
+   print("Classification report for testing set: ")
+   print(places_classificationReport)
+
+   print("Accuracy score for testing set: ")
+   print(accuracy_score(places_predicted_labels, places_test_labels))
+
+def classifyPlacesDatelines():
 
 	places_data,places_labels=get_data('reduced_data/reduced_datelines.out','reduced_data/places_labels.out')
 	places_train_data=places_data[0:11000]
@@ -70,5 +96,35 @@ def main():
 	print("Accuracy score for testing set: ")
 	print(accuracy_score(places_predicted_labels, places_test_labels))
 
+
+def classifyTopicsTitles():
+		topic_data,topic_labels=get_data('reduced_data/reduced_titles.out','reduced_data/topics_labels.out')
+		topic_train_data=topic_data[0:11000]
+		topic_test_data=topic_data[11001:11366]
+		topic_train_labels=topic_labels[0:11000]
+		topic_test_labels=topic_labels[11001:11366]
+
+		topic_vectorizer = CountVectorizer()
+		topic_X = topic_vectorizer.fit_transform(topic_train_data)
+		topic_Y= topic_vectorizer.transform(topic_test_data)
+
+		neigh = KNeighborsClassifier(n_neighbors=6)
+		neigh.fit(topic_X, topic_train_labels)
+
+		topic_predicted_labels=neigh.predict(topic_Y)
+
+		print(topic_predicted_labels)
+		topic_classificationReport = classification_report(topic_predicted_labels, topic_test_labels)
+		print("Classification report for testing set: ")
+		print(topic_classificationReport)
+
+		print("Accuracy score for testing set: ")
+		print(accuracy_score(topic_predicted_labels, topic_test_labels))
+def main():
+
+	# call classify bodies function in main to classiffy the bodies
+	classifyBodiesTopicsPlaces()
+	classifyTopicsTitles()
+	classifyPlacesDatelines()
 
 main()
