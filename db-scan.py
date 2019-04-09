f814d08748b5c99d40b1fb5d6d04a19395812f4e
LarryTheKing <lartking@gmail.com>
2019-04-09 19:38:42 -0400

    dbscan work done

--- db-scan.py
+++ db-scan.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
@@ -7,10 +8,16 @@ from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 from sklearn.decomposition import TruncatedSVD
 
+SKIP_ELBOW=True
+PLOT_COMPONENTS = 2
 N_CLUSTERS = 10
 N_COMPONENTS = 18
 MAX_SAMP = 400
-SAMP_STEP = 33
+MIN_SAMP = 5
+STEP_SAMP = 33
+MAX_EPS = 1.0
+MIN_EPS = 0.1
+STEP_EPS = 0.1
 
 def get_data(data_filename,labels_filename):
     data_holder=np.genfromtxt(data_filename,dtype='str',delimiter='\n')
@@ -25,21 +32,24 @@ def get_data(data_filename,labels_filename):
             labels.append(l[0])
     return data,labels
 
-def cluster(vector_data, labels, min_samples):
+def cluster(vector_data, labels, min_samples, eps):
     # svd = TruncatedSVD(n_components = N_COMPONENTS, n_iter=10, random_state=42, tol=0.0)
     # svd_data = svd.fit_transform(vector_data)
     svd_data=vector_data
-    estimator = DBSCAN(eps=0.5, min_samples=min_samples)
+    estimator = DBSCAN(eps=eps, min_samples=min_samples, n_jobs=-1)
     dbscan = estimator.fit(svd_data)
     assigned = dbscan.labels_
     h_score = metrics.homogeneity_score(labels, assigned)
     c_score = metrics.completeness_score(labels, assigned)
     return h_score, c_score, dbscan, svd_data
 
-def plot_clustering(vector_data, labels, min_samples):
-    h_score, c_score, dbscan, s_data = cluster(vector_data, labels, min_samples)
+def plot_clustering(vector_data, labels, min_samples, eps):
+    h_score, c_score, dbscan, s_data = cluster(vector_data, labels, min_samples, eps)
 
-    plot_svd = TruncatedSVD(n_components=2, n_iter=10, random_state=42, tol=0.0)
+    print('h score: ', h_score)
+    print('c score: ', c_score)
+
+    plot_svd = TruncatedSVD(n_components=PLOT_COMPONENTS, n_iter=10, random_state=42, tol=0.0)
     plot_svd_data = plot_svd.fit_transform(s_data)
 
     # Step size of the mesh. Decrease to increase the quality of the VQ.
@@ -48,6 +58,7 @@ def plot_clustering(vector_data, labels, min_samples):
 
     x_min, x_max = plot_svd_data[:, 0].min() - margin, plot_svd_data[:, 0].max() + margin
     y_min, y_max = plot_svd_data[:, 1].min() - margin, plot_svd_data[:, 1].max() + margin
+    #z_min, z_max = plot_svd_data[:, 2].min() - margin, plot_svd_data[:, 2].max() + margin
 
     label_index = {}
     for label in dbscan.labels_:
@@ -56,84 +67,126 @@ def plot_clustering(vector_data, labels, min_samples):
 
     cmap = plt.get_cmap('Set1')
 
-    plt.figure(1)
+    fig = plt.figure(1)
     plt.clf()
+    ####
+    # ax=fig.add_subplot(111,projection='3d')
+    # ax.scatter(plot_svd_data[:,0], plot_svd_data[:,1], plot_svd_data[:,2], s=2,
+    #     c=dbscan.labels_,
+    #     cmap=cmap)
+    # ax.set_title('DBSCAN clustering (3D SVD-projected plot)\n{} min-samples and {} epsilon radius'.format(min_samples, eps))
+    # ax.set_xlim(x_min, x_max)
+    # ax.set_ylim(y_min, y_max)
+    #ax.set_zlim(z_min, z_max)
+    #####
+
     plt.scatter(plot_svd_data[:,0], plot_svd_data[:,1], s=2,
         c=dbscan.labels_,
         cmap=cmap)
-    plt.title('DBSCAN clustering with {} min-samples (2D SVD-projected plot)\n'
-    'Pricipal components are marked with a colored cross'.format(min_samples))
+    plt.title('DBSCAN clustering (2D SVD-projected plot)\n{} min-samples and {} epsilon radius'.format(min_samples, eps))
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
+    #####
+
     plt.show()
 
 def vector_data(filename,labels_filename):
-    data,labels=get_data(filename,labels_filename)
-    data_vectorizer = CountVectorizer()
-    vector_data = data_vectorizer.fit_transform(data)
-    if(vector_data.shape[1]<N_COMPONENTS):
-        width=N_COMPONENTS-vector_data.shape[1]
-        vector_data=np.pad(vector_data,(0,width),'constant',constant_values=(0))
+	data,labels=get_data(filename,labels_filename)
+	data_vectorizer = CountVectorizer()
+	vector_data = data_vectorizer.fit_transform(data)
+	svd = TruncatedSVD(n_components = N_COMPONENTS, n_iter=10, random_state=42, tol=0.0)
+	svd_data = svd.fit_transform(vector_data)
+	return svd_data,labels
 
-    svd = TruncatedSVD(n_components = N_COMPONENTS, n_iter=10, random_state=42, tol=0.0)
-    svd_data = svd.fit_transform(vector_data)
-    return svd_data,labels
-
-def combine_vectors(weights,unique,bodies,orgs,people,exchanges,dates):
-    weighted_unique=np.multiply(0.1,unique)
-    weighted_people=np.multiply(0.1,people)
-    weighted_orgs=np.multiply(0.2,orgs)
-    weighted_dates=np.multiply(0.3,dates)
-    weighted_exchanges=np.multiply(0.7,exchanges)
-    weighted_bodies=np.multiply(0.5,bodies)
+def combine_vectors(type,unique,bodies,orgs,people,exchanges,dates):
+	if(type=='places'):
+		weighted_unique=np.multiply(0.1,unique)
+		weighted_people=np.multiply(0.9,people)
+		weighted_orgs=np.multiply(0.03,orgs)
+		weighted_dates=np.multiply(0.03,dates)
+		weighted_exchanges=np.multiply(0.03,exchanges)
+		weighted_bodies=np.multiply(0.5,bodies)
 
 
-    together_1=np.add(weighted_people,weighted_orgs)
-    together_2=np.add(weighted_dates,weighted_exchanges)
-    together_3=np.add(together_1,together_2)
+		together_1=np.add(weighted_people,weighted_orgs)
+		together_2=np.add(weighted_dates,weighted_exchanges)
+		together_3=np.add(together_1,together_2)
 
-    group_together_1=np.add(weighted_unique,weighted_bodies)
-    group_together_2=np.multiply(0.4,together_3)
+		group_together_1=np.add(weighted_unique,weighted_bodies)
+		group_together_2=np.multiply(0.4,together_3)
 
-    together=np.add(group_together_1,group_together_2)
-    return together
+		together=np.add(group_together_1,group_together_2)
+		return together
+	else:
+		weighted_unique=np.multiply(0.5,unique)
+		weighted_people=np.multiply(0.25,people)
+		weighted_orgs=np.multiply(0.05,orgs)
+		weighted_dates=np.multiply(0.25,dates)
+		weighted_exchanges=np.multiply(0.25,exchanges)
+		weighted_bodies=np.multiply(0.3,bodies)
+
+
+		together_1=np.add(weighted_people,weighted_orgs)
+		together_2=np.add(weighted_dates,weighted_exchanges)
+		together_3=np.add(together_1,together_2)
+
+		group_together_1=np.add(weighted_unique,weighted_bodies)
+		group_together_2=np.multiply(0.2,together_3)
+
+		together=np.add(group_together_1,group_together_2)
+		return together
 
 
 def main():
 
-    title_vectors,title_labels=vector_data('reduced_data/reduced_titles.out','reduced_data/topics_labels.out')
-    bodies_vectors,bodies_labels=vector_data('reduced_data/reduced_bodies.out','reduced_data/topics_labels.out')
-    orgs_vectors,orgs_labels=vector_data('reduced_data/reduced_orgs.out','reduced_data/topics_labels.out')
-    people_vectors,people_labels=vector_data('reduced_data/reduced_people.out','reduced_data/topics_labels.out')
-    exchanges_vectors,exchanges_labels=vector_data('reduced_data/reduced_exchanges.out','reduced_data/topics_labels.out')
-    dates_vectors,dates_labels=vector_data('reduced_data/reduced_dates.out','reduced_data/topics_labels.out')
-    weights=[]
-    combined_svd_vector=combine_vectors(weights,title_vectors,bodies_vectors,orgs_vectors,people_vectors,exchanges_vectors,dates_vectors)
-    # data_vectorizer = CountVectorizer()
-    # vector_data = data_vectorizer.fit_transform(data)
+    type='topics'
+    if(type=='topics'):
+        title_vectors,title_labels=vector_data('reduced_data/reduced_titles.out','reduced_data/topics_labels.out')
+        bodies_vectors,bodies_labels=vector_data('reduced_data/reduced_bodies.out','reduced_data/topics_labels.out')
+        orgs_vectors,orgs_labels=vector_data('reduced_data/reduced_orgs.out','reduced_data/topics_labels.out')
+        people_vectors,people_labels=vector_data('reduced_data/reduced_people.out','reduced_data/topics_labels.out')
+        exchanges_vectors,exchanges_labels=vector_data('reduced_data/reduced_exchanges.out','reduced_data/topics_labels.out')
+        dates_vectors,dates_labels=vector_data('reduced_data/reduced_dates.out','reduced_data/topics_labels.out')
+        combined_svd_vector=combine_vectors('topics',title_vectors,bodies_vectors,orgs_vectors,people_vectors,exchanges_vectors,dates_vectors)
+        n_labels = len(set(title_labels))
+        labels=title_labels
+    else:
+        dateline_vectors,dateline_labels=vector_data('reduced_data/reduced_titles.out','reduced_data/places_labels.out')
+        bodies_vectors,bodies_labels=vector_data('reduced_data/reduced_bodies.out','reduced_data/places_labels.out')
+        orgs_vectors,orgs_labels=vector_data('reduced_data/reduced_orgs.out','reduced_data/places_labels.out')
+        people_vectors,people_labels=vector_data('reduced_data/reduced_people.out','reduced_data/places_labels.out')
+        exchanges_vectors,exchanges_labels=vector_data('reduced_data/reduced_exchanges.out','reduced_data/places_labels.out')
+        dates_vectors,dates_labels=vector_data('reduced_data/reduced_dates.out','reduced_data/places_labels.out')
+        combined_svd_vector=combine_vectors('places',dateline_vectors,bodies_vectors,orgs_vectors,people_vectors,exchanges_vectors,dates_vectors)
+        n_labels = len(set(dateline_labels))
+        labels=dateline_labels
 
-    n_labels = len(set(title_labels))
-
-    k_values = []
+    e_values = []
+    s_values = []
     h_scores = []
     c_scores = []
-    for i in range(5, MAX_SAMP, SAMP_STEP):
-        h_score, c_score, dbscan, svd_data = cluster(combined_svd_vector, title_labels, i)
-        k_values.append(i)
-        h_scores.append(h_score)
-        c_scores.append(c_score)
+    if not SKIP_ELBOW:
+        for e in np.arange(MIN_EPS, MAX_EPS, STEP_EPS):
+            for s in range(MIN_SAMP, MAX_SAMP, STEP_SAMP):
+                h_score, c_score, dbscan, svd_data = cluster(combined_svd_vector, labels, s, e)
+                e_values.append(e)
+                s_values.append(s)
+                h_scores.append(h_score)
+                c_scores.append(c_score)
 
     # Plot h scores vs k
-    plt.figure(1)
-    plt.clf()
-    plt.title('DBSCAN Homogeneity vs Min-Samples\nTitle Vectors and Topics Labels')
-    plt.xlabel('Min Sample Count')
-    plt.ylabel('Homogeneity')
-    plt.plot(k_values, h_scores, '-', lw=2)
+    fig = plt.figure(1)
+    ax=fig.add_subplot(111,projection='3d')
+    ax.set_title('DBSCAN Homogeneity vs Min-Samples & Epislon Radius \nFeature Vectors and Places Labels')
+    ax.set_xlabel('Epsilon Radius')
+    ax.set_ylabel('Min-Samples')
+    ax.set_zlabel('Homogeneity')
+    ax.scatter(e_values, s_values, h_scores, s=2)
     plt.show()
 
-    n_clusters = int(input('min-samples to use for plot:'))
-    plot_clustering(combined_svd_vector, title_labels, n_clusters)
+    min_samples = int(input('min-samples to use for plot:'))
+    eps = float(input('eps to use for plot:'))
+    plot_clustering(combined_svd_vector, labels, min_samples, eps)
 
 main()
 
